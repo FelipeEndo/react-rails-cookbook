0f729b4695740ffde1888f2181353523ab9d7dd9
Felipe Endo <felipe.endo@rebase.com.br>
2020-09-25 03:59:53 -0300

    Fix routes, event listener and add react configs

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -6,8 +6,7 @@ import Popper from 'popper.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import App from "../components/App"
 
-
-document.addEventListenecer("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   render(
     <App />,
     document.body.appendChild(document.createElement("div"))
